bae702dd2025c689dd5b448652f3e37369547c74
AmneetSinghh <amneet@vahan.co>
2021-09-08 06:48:51 +0545

    First commmit

---
+++ src/main/java/com/codewithharry/user_service_bro/UserServiceBroApplication.java
@@ -0,0 +1,14 @@
+package com.codewithharry.user_service_bro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceBroApplication {
+
+	public static void main(String[] args) {
+		System.out.print("this si the user inrterace!~");
+		SpringApplication.run(UserServiceBroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithharry/user_service_bro/UserServiceBroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithharry.user_service_bro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceBroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
